f6fe0caf705e2cab0306b4c79affa9d1c7ebb40a
Remi <37759218+RemiDayon@users.noreply.github.com>
2021-12-01 10:17:09 +0100

    Add files via upload

---
+++ Banner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Banner : MonoBehaviour
+{
+    [SerializeField] Material mat = null;
+    [SerializeField] Texture2D tex = null;
+    float noisyTime = 0f;
+    float noise = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetFloat("pivot", transform.position.y);
+        noise = tex.GetPixel((int)(Time.timeSinceLevelLoad * .25f), 0).r;
+        noisyTime += Time.deltaTime + noise;
+        mat.SetFloat("noise", noise);
+        mat.SetFloat("noisyTime", noisyTime);
+    }
+}
